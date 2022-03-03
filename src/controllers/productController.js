1037ee65b4358c0da45e32bc04c17bb3114da997
Yara Tavares <yaracriistavares@gmail.com>
2022-03-03 16:06:03 -0300

    feat: products router

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,7 +3,7 @@ import connection from "../db.js";
 export async function getProducts(req, res) {
   try {
     const resultProducts = await connection.query("SELECT * FROM produtos");
-    const products = resultProducts.rows[0];
+    const products = resultProducts.rows;
 
     res.status(200).send(products);
   } catch (err) {
@@ -13,37 +13,97 @@ export async function getProducts(req, res) {
 }
 
 export async function getProduct(req, res) {
-  res.sendStatus(200);
-}
-
-export async function addProduct(req, res) {
-  const { user } = res.locals;
   const { id } = req.params;
 
-  if (!id) {
-    res.sendStatus(404);
-  }
-
   try {
-    const resultProductId = await connection.query(
+    const resultProduct = await connection.query(
       "SELECT * FROM produtos WHERE id = $1",
       [id]
     );
-    if (resultProductId.rowCount === 0) {
-      res.sendStatus(404);
-    }
+    const product = resultProduct.rows[0];
+
+    res.status(200).send(product);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
   }
+}
 
-  res.sendStatus(200);
+export async function addProduct(req, res) {
+  const { user } = res.locals;
+  const newProduct = req.body;
+
+  try {
+    await connection.query(
+      `INSERT INTO produtos (nome, preco, "idUsuario") VALUES ($1, $2, $3)`,
+      [newProduct.nome, newProduct.preco, user.id]
+    );
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
 
 export async function updateProduct(req, res) {
-  res.sendStatus(200);
+  const { id } = req.params;
+  const { user } = res.locals;
+  const { nome, preco } = req.body;
+
+  try {
+    const productId = await connection.query(
+      "SELECT * FROM produtos WHERE id=$1",
+      [id]
+    );
+    if (productId.rowCount === 0) {
+      return res.sendStatus(404);
+    }
+    const productUser = await connection.query(
+      `SELECT * FROM produtos WHERE id=$1 AND "idUsuario"=$2`,
+      [id, user.id]
+    );
+    if (productUser.rowCount === 0) {
+      return res.sendStatus(401);
+    }
+
+    await connection.query(
+      `UPDATE produtos SET nome=$1, preco=$2 WHERE id=$3 AND "idUsuario" = $4 `,
+      [nome, preco, id, user.id]
+    );
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
 
 export async function deleteProduct(req, res) {
-  res.sendStatus(200);
+  const { id } = req.params;
+  const { user } = res.locals;
+
+  try {
+    const productId = await connection.query(
+      "SELECT * FROM produtos WHERE id=$1",
+      [id]
+    );
+    if (productId.rowCount === 0) {
+      return res.sendStatus(404);
+    }
+    const productUser = await connection.query(
+      `SELECT * FROM produtos WHERE id=$1 AND "idUsuario"=$2`,
+      [id, user.id]
+    );
+    if (productUser.rowCount === 0) {
+      return res.sendStatus(401);
+    }
+
+    await connection.query(
+      `DELETE FROM produtos WHERE id=$1 AND "idUsuario" = $2 `,
+      [id, user.id]
+    );
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -6,12 +6,18 @@ import {
   updateProduct,
   deleteProduct,
 } from "../controllers/productController.js";
+import validateSchemaMiddleware from "../middlewares/validateSchemaMiddleware.js";
+import productSchema from "../schemas/productSchema.js";
 
 const productRouter = Router();
 
 productRouter.get("/products", getProducts);
 productRouter.get("/products/:id", getProduct);
-productRouter.post("/products", addProduct);
+productRouter.post(
+  "/products",
+  validateSchemaMiddleware(productSchema),
+  addProduct
+);
 productRouter.put("/products/:id", updateProduct);
 productRouter.delete("/products/:id", deleteProduct);
 
